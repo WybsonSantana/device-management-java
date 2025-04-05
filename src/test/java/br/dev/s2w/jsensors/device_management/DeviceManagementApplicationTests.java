9a36b275fe035c08f64706a5b20c82b9fc6f0e41
WybsonSantana <wybson@hotmail.com>
2025-04-05 18:31:11 -0300

    JSensors: #01 - 08.08 - Multiprojects with IntelliJ IDEA

---
+++ src/test/java/br/dev/s2w/jsensors/device_management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.s2w.jsensors.device_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
